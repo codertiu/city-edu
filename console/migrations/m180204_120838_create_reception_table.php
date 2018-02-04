1af0ce7554a39b528b76a27080de656d5b67ff67
Iskandarcoder <Iskandarcoder@gmail.com>
2018-02-04 18:54:35 +0500

    hi15

---
+++ console/migrations/m180204_120838_create_reception_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `reception`.
+ */
+class m180204_120838_create_reception_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('reception', [
+            'id' => $this->primaryKey(),
+            'edu_center_id' => $this->integer()->notNull(),
+            'fio' => $this->string(255)->notNull(),
+            'tel' => $this->string(35)->unique()->notNull(),
+            'coming_id' => $this->integer()->notNull(),
+            'type_edu_id' => $this->integer()->notNull(),
+            'date_coming' => $this->dateTime()->notNull(),
+            'creater' => $this->integer()->notNull(),
+            'create_date' => $this->integer()->notNull(),
+            'update_date' => $this->integer()->notNull(),
+            'instance_id' => $this->integer()->notNull(),
+            'comment_id' => $this->integer()->Null(),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('reception');
+    }
+}
