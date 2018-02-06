1ddbd6986241fdb59b408499c06a5412b5cfc23d
fozzza <webfoza@gmail.com>
2018-02-06 23:34:59 +0500

    forma

--- backend/views/schedule/_form.php
+++ backend/views/schedule/_form.php
@@ -3,9 +3,6 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-/* @var $this yii\web\View */
-/* @var $model backend\models\Schedule */
-/* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="schedule-form">
